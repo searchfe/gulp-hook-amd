a2c1e1d0925df64bd8aaa7e41869f4c26036a59a
lijie0981 <974277025@qq.com>
2019-10-22 14:56:28 +0800

    add cache

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import Parser from './parser';
 import { Transform } from 'gulp-transform-cache';
 import { File, PluginError } from 'gulp-util';
 export { amdUrlParser } from './amdUri';
-class Pipe extends Transform {}
+class ApmParser extends Transform {}
 export function apmParser(option: any) {
     let parser = Parser.create(option.projectPath);
-    return new Pipe({
+    return new ApmParser({
         objectMode: true,
         transform: (file: File, enc, callback: Function) => {
             if (file.isNull()) {
